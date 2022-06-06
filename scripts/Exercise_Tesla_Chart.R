114d41a839821999127a3a71ba84ab5bc2058d0d
pizzawarrior <i.norstad@gmail.com>
2022-06-06 12:25:31 -0700

    new exercises

--- scripts/Exercise_Tesla_Chart.R
+++ scripts/Exercise_Tesla_Chart.R
@@ -13,18 +13,7 @@ DF <- read.csv("/Users/ME/Desktop/Tesla PS Charts - Fig 1.csv")
 #mutate(No.sci = format(Percent.of.Market.Cap...10x, 
    # scientific = FALSE, big.mark = ","))
 
-DF2<- DF %>% 
-mutate(rounded = signif(Percent.of.Market.Cap...10x, digits = 4))
-
 summary(DF$Percent.of.Market.Cap...10x)
-
-DF.date <- DF %>% 
-  mutate(clean_date = mdy(caldt))
-
-# Plot
-ggplot(DF.date, aes(x=clean_date, y=Percent.of.Market.Cap...10x)) +
-  scale_y_continuous(name="% of market cap 10x", limits=c(0, 0.3)) +
-  geom_line()
 
 #how to determine class? (is the date operating as a date?)
 class(DF$caldt)
@@ -34,3 +23,13 @@ class(DF$Percent.of.Market.Cap...10x)
 str(DF.date)
 #caldt needs to be changed to a date format
 #may need to use scale in the ggplot call to get it to capture the decimals
+
+#Turn caldt column into useable date
+DF.date <- DF %>% 
+  mutate(clean_date = mdy(caldt)) %>% 
+  select(clean_date, Percent.of.Market.Cap...10x)
+
+# Plot
+ggplot(DF.date, aes(x=clean_date, y=Percent.of.Market.Cap...10x)) +
+  scale_y_continuous(name="% of market cap 10x", limits=c(0, 0.3)) +
+  geom_line()
